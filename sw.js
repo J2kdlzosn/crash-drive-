7baa1004351da15fbeb5644ce246441587a7b3c7
J2kdlzosn <linodamota@icloud.com>
2026-09-25 18:56:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='crash-drive-v5';const F=['./','./index.html','./game.html','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
